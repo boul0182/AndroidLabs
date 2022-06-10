36c9fda8dcedf4db3a481092c47cd8382efa096d
Maria Boulescu-Qureshi <boul0182@algonquinlife.com>
2022-06-10 19:50:48 -0400

    Lab3 shared_prefferences

--- app/src/main/java/com/cst2335/boul0182/MainActivity.java
+++ app/src/main/java/com/cst2335/boul0182/MainActivity.java
@@ -2,54 +2,88 @@ package com.cst2335.boul0182;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public final static String PREFERENCES_FILE = "MyData";
+    private static final String TAG = "MAIN_ACTIVITY";
+
+    TextView editTextEmail;
+//    TextView editTextPswd = findViewById(R.id.editTextPassword);
+
+    //SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        Log.e(TAG, "onCreate");
+        setContentView(R.layout.activity_main_shared_pref);
 
-        //now your xml is loaded
-        TextView topView = findViewById(R.id.textView); //must match XML id
-        String oldText = topView.getText().toString();
-        topView.setText("Java put this here");
+        SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
 
+        //Read preferences for email
+        editTextEmail = findViewById(R.id.editTextEmailAddress);
+        String previousEmail = prefs.getString("Email", "defaultEmail");
+        editTextEmail.setText(previousEmail);
 
-        Button btn = findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() { //anonymous class
-            @Override
-            public void onClick(View v) {
-                topView.setText("Edit text has " + btn.getText());
-            }
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(clk -> {
+
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            goToProfile.putExtra("Email", editTextEmail.getText().toString());
+
+            startActivity(goToProfile);
         });
+    }
 
-        Switch switchBotton = (Switch) findViewById(R.id.switch1);
+    @Override //screen is visible but not responding
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    }
 
-        switchBotton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+    @Override //screen is visible but not responding
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    }
 
-                Snackbar snackbar;
-                if (isChecked) {
-                    snackbar = Snackbar.make(buttonView, R.string.switch_msg_on,
-                            Snackbar.LENGTH_LONG).setAction("undo", click ->
-                            buttonView.setChecked(!isChecked));
-                } else {
-                    snackbar = Snackbar.make(buttonView, R.string.switch_msg_off, Snackbar.LENGTH_LONG);
-                }
-                snackbar.show();
-            }
-        });
+    @Override //screen is visible but not responding
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+
+        SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("Email", editTextEmail.getText().toString());
+        //prefsEditor.putString("USERINPUT", editTextPswd.getText().toString());
+
+        prefsEditor.apply(); //save to disk
+
+    }
+
+    @Override //not visible
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override  //garbage collected
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
     }
 }
---
+++ app/src/main/java/com/cst2335/boul0182/ProfileActivity.java
@@ -0,0 +1,119 @@
+package com.cst2335.boul0182;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.view.View;
+import android.util.Log;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    //public final static String PREFERENCES_FILE = "MyData";
+    private final static String TAG = "PROFILE_ACTIVITY";
+
+    //SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+
+    TextView editTextEmail;
+    ActivityResultLauncher<Intent> myPictureTakerLauncher;
+    ImageButton cameraButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        editTextEmail = findViewById(R.id.editTextEmailAddress);
+        //TextView editTextPswd = findViewById(R.id.editTextPassword);
+        Button loginButton = findViewById(R.id.loginButton);
+        cameraButton = findViewById(R.id.cameraButton);
+
+        //Read preferences for email
+        Intent fromMain = getIntent();
+        String retrievedEmail = fromMain.getStringExtra("Email");
+        editTextEmail.setText(retrievedEmail);
+
+
+
+        myPictureTakerLauncher =
+                registerForActivityResult( new ActivityResultContracts.StartActivityForResult()
+                        ,new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK)
+                { Intent data = result.getData();
+                    Bitmap imgbitmap = (Bitmap) data.getExtras().get("data");
+                    cameraButton.setImageBitmap(imgbitmap); // the imageBuLon
+                }
+                else if(result.getResultCode() == Activity.RESULT_CANCELED)
+                    Log.i(TAG, "User refused to capture a picture.");
+                }
+        } );
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                myPictureTakerLauncher.launch(takePictureIntent);
+            }
+    }
+
+
+
+        @Override //screen is visible but not responding
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart");
+    };
+
+    @Override //screen is visible but not responding
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume");
+    };
+
+    @Override //screen is visible but not responding
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause");
+    }
+
+    @Override //not visible
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop");
+    }
+
+    @Override  //garbage collected
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy");
+    }
+}
